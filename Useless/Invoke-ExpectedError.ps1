546ab2f884173d34f6f368f1bf84cf4cd2b157d4
Mischa Boender <mischa@boender.eu>
2017-08-14 21:39:10 +0200

    - Changed folder structure (again) - Added "Process" block to Select-FromList

--- Useless/Invoke-ExpectedError.ps1
+++
@@ -1,8 +0,0 @@
-function Invoke-ExpectedError {
-    param(
-        [string]$Message="User `"$env:USERNAME`" caused an unrecoverable expected error."
-    )
-    
-    Add-Type -AssemblyName PresentationFramework
-    [System.Windows.MessageBox]::Show($Message, "Expected Error", 0, 16)
-}
