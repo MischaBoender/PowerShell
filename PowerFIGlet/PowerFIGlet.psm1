7187c37359cb6f42ffd0da686b678bbedb6f8b06
Mischa Boender <mischa@boender.eu>
2017-08-18 00:57:49 +0200

    - Converted PowerFIGlet to a PSModule - Cleaned up the fonts a bit

--- PowerFIGlet/PowerFIGlet.psm1
+++ PowerFIGlet/PowerFIGlet.psm1
@@ -1,3 +1,49 @@
+$Global:PowerFIGletModuleDirectory = $PSScriptRoot
+
+function Convert-FIGlet {
+    param(
+        [Parameter(ValueFromPipeline=$true, Position=0)]
+        [object]$InputObject,
+        [string]$Font="Standard",
+        [string]$FontDirectory,
+        [int]$TerminalWidth=$Host.UI.RawUI.BufferSize.Width,
+        [switch]$Center
+    )
+
+    begin {
+        $FIGletFont = Get-FIGletFont -Font $Font -Directory $FontDirectory
+        $TerminalWidth += 1
+    }
+    
+    process {
+        $OutputLines = @()
+        for ($Line=0; $Line -lt $FIGletFont.Height; $Line++) {
+            $TotalWidth = 0
+
+            foreach ($Char in [char[]]($InputObject.ToString())) {
+                $CharLine = $FIGletFont.Characters[[int]$Char][$Line]
+                $TotalWidth += $CharLine.Length
+                $OutputLineIndex = [math]::Floor($TotalWidth / $TerminalWidth)
+                if (-not $OutputLines[$OutputLineIndex]) {$OutputLines += ,@("")}
+                if (-not $OutputLines[$OutputLineIndex][$Line]) {$OutputLines[$OutputLineIndex] += ""}
+                $OutputLines[$OutputLineIndex][$Line] += $CharLine
+            }
+        }
+
+        for ($i=0; $i -lt $OutputLines.Count; $i++) {
+            if ($Center.IsPresent) {
+                $LineWidth = $OutputLines[$i][0].Length
+                $Prepend = ($TerminalWidth - $LineWidth) /2
+                Write-Output ([string]::Join("`r`n", ($OutputLines[$i] | %{(" " * $Prepend) + $_})))
+            }
+            else {
+                Write-Output ([string]::Join("`r`n", $OutputLines[$i]))
+            }
+        }
+    }
+}
+
+
 function Get-FIGletFont {
     [CmdletBinding(DefaultParameterSetName="Font")]
     param(
@@ -5,7 +51,7 @@ function Get-FIGletFont {
         [string]$Font="standard",
         [Parameter(ParameterSetName="ListAvailable")]
         [switch]$ListAvailable,
-        [string]$Directory=$PWD
+        [string]$FontDirectory=$Global:PowerFIGletModuleDirectory
     )
 
     process {
@@ -90,3 +136,24 @@ function Get-FIGletFont {
         }
     }
 }
+
+
+function Test-FIGlet {
+    param(
+        [string]$Text
+    )
+    
+    Get-FigletFont -ListAvailable | %{
+        try {
+        Write-Host "Font: $($_.Font)"
+        if (-not $Text) {
+            ConvertTo-FIGlet -InputObject $_.Font -Font $_.Font
+        }
+        else {
+            ConvertTo-FIGlet -InputObject $Text -Font $_.Font
+        }
+        } catch {}
+    }
+}
+
+Export-ModuleMember -Function *
